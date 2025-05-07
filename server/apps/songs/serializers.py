a0ae07e908e04891fd7a939241ee3f9069745d81
TruongDHiep <truonghiep051103@gmail.com>
2025-05-07 13:51:07 +0700

    edit lay data album

--- server/apps/songs/serializers.py
+++ server/apps/songs/serializers.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
 from .models import Song
 from apps.songs.services import SongService
+from apps.albums.serializers import AlbumSerializer
+from apps.albums.models import Album
+from apps.artists.models import Artist
+from apps.artists.serializers import ArtistSerializer
+
 class SongSerializer(serializers.ModelSerializer):
     file = serializers.FileField(write_only=True, required=False)
     img_upload = serializers.FileField(write_only=True, required=False)
     video_upload = serializers.FileField(write_only=True, required=False)
+    album = AlbumSerializer(read_only=True)  
+    album_id = serializers.PrimaryKeyRelatedField(
+        queryset=Album.objects.all(), source='album', write_only=True
+    )
+    artist = ArtistSerializer(read_only=True)
+    artist_id = serializers.PrimaryKeyRelatedField(
+        queryset=Artist.objects.all(), source='artist', write_only=True
+    )
+    
 
     class Meta:
         model = Song
