2c7dcd3e69ec5e76ca44e6c9f25ddcd42ac5bc9b
g1akhanh <3123411148@sv.sgu.edu.vn>
2025-05-15 00:59:49 +0700

    album va artist lay anh bang AWS

--- server/apps/artists/models.py
+++ server/apps/artists/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Artist(models.Model):
     name = models.CharField(max_length=100)
-    avatar = models.TextField()
+    avatar = models.TextField(blank=True, null=True)
     created_at = models.DateField(auto_now_add=True)
     description = models.TextField(blank=True)
 
